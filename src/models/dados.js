4d384a2aa97a1029b2a3f9fe53f3e6cda18cc59a
Ana Cremasco <anaclara.luizcremasco@gmail.com>
2025-09-22 13:56:00 -0300

    dados

---
+++ src/models/dados.js
@@ -0,0 +1,114 @@
+const influencers = [
+    {
+        id: 1,
+        nome: "Whindersson Nunes",
+        nicho: "Comédia e Entretenimento",
+        plataforma:"YouTube, Instagram, TikTok",
+        seguidores: "59 milhões",
+        engajamento: "Alto",
+        ultimoPost: "Conteúdo regular",
+        pais: "Brasil",
+        ativo: "Ativo"
+    },
+    {
+        id: 2,
+        nome: "Camila Coutinho",
+        nicho: "Moda e Lifestyle",
+        plataforma:"Instagram, Blog",
+        seguidores: "3 milhões",
+        engajamento: "Alto",
+        ultimoPost: "Moda e eventos",
+        pais: "Brasil",
+        ativo: "Ativa"
+    },
+    {
+        id: 3,
+        nome: "Felipe Neto",
+        nicho: "Entretenimento e Opinião",
+        plataforma:"YouTube, Instagram, X (Twitter)",
+        seguidores: "46 milhões",
+        engajamento: "Alto",
+        ultimoPost: "Vídeos e posts diários",
+        pais: "Brasil",
+        ativo: "Ativo" 
+    },
+    {
+        id: 4,
+        nome: "Virginia Fonseca",
+        nicho: "Beleza, Lifestyle e Maternidade",
+        plataforma:"Instagram, YouTube, TikTok",
+        seguidores: "47 milhões",
+        engajamento: "Alto",
+        ultimoPost: "Publicações diárias",
+        pais: "Brasil",
+        ativo: "Ativo" 
+    },
+    {
+        id: 5,
+        nome: "Erico Rocha",
+        nicho: "Marketing Digital e Empreendedorismo",
+        plataforma:"Instagram, YouTube",
+        seguidores: "2,7 milhões",
+        engajamento: "Alto",
+        ultimoPost: "Lançamentos e negócios.",
+        pais: "Brasil",
+        ativo: "Ativo" 
+    },
+    {
+        id: 6,
+        nome: "Mari Maria",
+        nicho: "Beleza e Maquiagem",
+        plataforma:"Instagram, YouTube, TikTok",
+        seguidores: "25 milhões",
+        engajamento: "Alto",
+        ultimoPost: "Conteúdo regular",
+        pais: "Brasil",
+        ativo: "Ativa" 
+    },
+    {
+        id: 7,
+        nome: "Casimiro Miguel",
+        nicho: "Esportes e Entretenimento",
+        plataforma:"Twitch, YouTube, Instagram",
+        seguidores: "3,6 milhões",
+        engajamento: "Alto",
+        ultimoPost: "lives e cortes de vídeos",
+        pais: "Brasil",
+        ativo: "Ativo" 
+    },
+    {
+        id: 8,
+        nome: "Pâmela Drudi",
+        nicho: "Lifestyle, Beleza e Humor",
+        plataforma:"TikTok, Instagram",
+        seguidores: "17 milhões",
+        engajamento: "Alto",
+        ultimoPost: "vídeos curtos e divertidos",
+        pais: "Brasil",
+        ativo: "Ativa" 
+    },
+    {
+        id: 9,
+        nome: "Alok",
+        nicho: "Música e DJ",
+        plataforma:"Instagram, YouTube, Spotify",
+        seguidores: "28 milhões",
+        engajamento: "Alto",
+        ultimoPost: "anúncios de eventos e lançamentos",
+        pais: "Brasil",
+        ativo: "Ativo" 
+    },
+    {
+        id: 10,
+        nome: "Jout Jout",
+        nicho: "Opinião e Comportamento",
+        plataforma:"YouTube, Instagram",
+        seguidores: "2,4 milhões",
+        engajamento: "Alto",
+        ultimoPost: "frequência menos intensa",
+        pais: "Brasil",
+        ativo: "Ativo" 
+    }
+]
+
+export default { influencers }
